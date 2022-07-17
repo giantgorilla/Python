e9e2a0073846118ff742ac9860118825ba0995a5
Doğukan <50502529+giantgorilla@users.noreply.github.com>
2022-07-17 16:40:44 +0300

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,103 @@
+
+                                                                      #EN
+
+# Hello Guys, today we are starting Python with simple example.
+
+# We can start with a classic example that happens in every programming language.
+
+print("Hello World")
+#If u use this code you can see Hello World print out at the down of code.
+
+# You can write this print out different ways on Python but this step a little bit will harder than first example.
+# Because u are using input so you need to take information from user.
+# Come on! I'll show you how you will do this.
+
+
+# First of all you should variable to identify so it's all up to you.
+
+a = input("Write the sentence u want to print here")
+print(a)
+
+# a = It's my variable but u can change it according to you (but be careful here because you can use just string data type) 
+
+# I will show, if u have 2 print out how u will leave a space.
+# It has two ways like last example you can do like that or.. like that.
+
+# It's easy example
+
+a = "Hello World\n"
+b = "What can i do today for myself ?"
+
+print(a)
+print(b)
+
+# It's a little bit harder than above step.
+
+a = input("Write first sentence u want to print here\n")
+b = input("Write second sentence u want to print here")
+print(a)
+print(b)
+
+# If u dont know how do u put this symbol "\n" it's so easy. You have to press together ALT GR + *. but There is a IMPORTANT!!
+# You should be careful because you will confuse with this symbol "/" so i am warning to you at the this time.
+
+# FQA 
+# What does it mean string ? String data type is traditionally a sequence of characters, either as a literal constant or as some kind of variable
+# When does it work "\n for us? If u want to leave a space you can use this symbol.
+# If u read it here thanks to u, i hope help to you!
+
+                                                                      #TR
+
+# Herkese merhaba arkadaşlar, bugün Python'a basit bir örnekle giriş yapacağız.
+
+# Her yazılım dilinde olan klasik bir örnekle başlayalım.
+
+print("Hello World")
+
+# Eğer ki bu kodu kullanırsanız içeriye yazdığınız yazıyı kodun altındaki çıktıda göreceksiniz.
+# Pythonda aynı çıktıya erişmenin birden fazla yolu vardır ama bu adım ilk örneğe göre biraz daha zordur.
+# Bilgiyi kullancıdan alacağımız için input kullanacağız.
+# Hadi gelin bunu size nasıl yapacağınızı göstereyim.
+
+# Öncelikle bir tane değişken belirlemelisiniz, belirleyeceğiniz değişken size kalmış.
+
+a = input("Çıktısını almak istediğiniz cümleyi giriniz.")
+print(a)
+
+# a = Ben kendi değişkenimi a olarak belirledim bunu keninize göre değiştirebilirsiniz ama burada dikkat etmeniz gereken bir nokta var. Belir-
+#lediğiniz değişken string olmalıdır yani sadece harf kullanabilirsiniz.
+
+# Eğer ki iki adet çıktınız var ise iki çıktı arasında nasıl boşluk bırakacağını göstereceğim.
+# Önceki örnekte olduğu gibi bunun da iki farklı yolu var.
+
+# Kolay Örnek
+
+a = "Hello World\n"
+b = "What can i do today for myself ?"
+
+print(a)
+print(b)
+
+# Üstteki örneğin biraz daha zoru
+
+a = input("Write first sentence u want to print here\n")
+b = input("Write second sentence u want to print here")
+
+print(a)
+print(b)
+
+# "\n" bu sembolü nasıl yapacağınızı bilmiyorsanız ALT GR + * tuşlarına birlikte basarak bu sembolü yapabilirsiniz. Buraya dikkat edin!!
+# Bu sembolü "/n" ile karıştıracaksınız bu yüzden şimdiden sizi uyarıyorum.
+
+# SSS
+# String ne demektir ? String sadece harflerden oluşan bir data tipidir. Bu data tipinde hiçbir şekilde sayı kullanılmaz.
+# \n ne işimize yarıyor ? \n iki çıktı arasında boşluk bırakmanızı, daha doğrusu bir alt satıra inmenizi sağlar.
+
+# Buraya kadar gelip okuduysanız ve faydalandıysanız teşekkür ederim, umarım yardımcı olmuşumdur. 
+
+
+
+
+
+
+
